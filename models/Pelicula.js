2f2174c9770e6dbd1fa3fb0ac97f2d8aa45295eb
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-02-25 21:12:20 -0600

    modelo Peliculas creado

---
+++ models/Pelicula.js
@@ -0,0 +1,35 @@
+//Pelicula.js
+
+// Clase que represneta un registro de película que sera modificado por varios usuarios.
+
+class Pelicula {
+	constructor(nombre, id, duracion, genero, calificación, sinopsis, director, año) {
+		this.nombre = nombre; //Nombre de la película
+		this.id = id; //ID de la película
+		this.duracion = duracion; //duracion de la película
+		this.genero = genero; // genero de la película
+		this.calificacion = calificación; //calificaciones otorgadas por los usuarios
+		this.sinopsis = sinopsis; //descripcion de la película
+		this.director = director; //director de la película
+		this.año = año; //año de estreno de la película
+	}
+
+	guardar() {
+		//función para guardar un registro nuevo en la base de datos
+	}
+
+	obtener() {
+		//función para obtener un / multiples registros de la base de datos
+	}
+
+	eliminar() {
+		//función para eliminar un registro
+	}
+
+	modificar() {
+		//función para modificar propiedades de un registro
+	}
+}
+
+//exporta la clase
+module.exports = Pelicula;
